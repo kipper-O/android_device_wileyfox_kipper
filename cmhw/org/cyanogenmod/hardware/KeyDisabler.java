c8a619be58509605c85de077b6223b1f40624238
Michael Bestas <mikeioannina@gmail.com>
2017-01-14 14:34:13 +0000

    kipper: cmhw: Update and clean up KeyDisabler
     * Update copyright
     * Use new CMSDK FileUtils
     * Mark String constants as static final
     * Actually check the file path in isSupported()
     * Prevent null pointer exceptions when reading the node
     * Clean up formatting
    
    Change-Id: Ief64e211fcfecbb220cf871dea8af53478932b2f

--- cmhw/org/cyanogenmod/hardware/KeyDisabler.java
+++ cmhw/org/cyanogenmod/hardware/KeyDisabler.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +17,7 @@
 
 package org.cyanogenmod.hardware;
 
-import org.cyanogenmod.hardware.util.FileUtils;
+import org.cyanogenmod.internal.util.FileUtils;
 
 /*
  * Disable capacitive keys
@@ -28,18 +29,19 @@ import org.cyanogenmod.hardware.util.FileUtils;
  */
 
 public class KeyDisabler {
-
-    private static String CONTROL_PATH =
+    private static final String CONTROL_PATH =
         "/sys/devices/soc.0/78b9000.i2c/i2c-5/5-0024/main_ttsp_core.cyttsp4_i2c_adapter/btn_enabled";
 
-    public static boolean isSupported() { return true; }
+    public static boolean isSupported() {
+        return FileUtils.isFileReadable(CONTROL_PATH) &&
+                FileUtils.isFileWritable(CONTROL_PATH);
+    }
 
     public static boolean isActive() {
-        return (FileUtils.readOneLine(CONTROL_PATH).equals("0"));
+        return "0".equals(FileUtils.readOneLine(CONTROL_PATH));
     }
 
     public static boolean setActive(boolean state) {
-        return FileUtils.writeLine(CONTROL_PATH, (state ? "0" : "1"));
+        return FileUtils.writeLine(CONTROL_PATH, state ? "0" : "1");
     }
-
 }
