37a9180144643976e2b545161182437630093e0b
p-sdev <mahmod115.ma@gmail.com>
2017-10-05 02:16:15 +0300

    Revert "Dont build camera for now [2]"
    This reverts commit 1e5a692066d0ed868505cc908928103238cf77f3.

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -1,10 +1,10 @@
 # Camera
 PRODUCT_PACKAGES += \
-#    android.hardware.camera.provider@2.4-impl \
-#    camera.device@3.2-impl \
-#    camera.msm8916 \
-#    libmm-qcamera \
-     Snap
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@3.2-impl \
+    camera.msm8916 \
+    libmm-qcamera \
+    Snap
 
 # Permissions
 PRODUCT_COPY_FILES += \
