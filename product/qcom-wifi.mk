bc7a897ecd169e7447f00cbfe4c2b631f8c5c422
p-sdev <mahmod115.ma@gmail.com>
2017-10-05 02:02:12 +0300

    Revert "crackling: Add HIDL manifest"
    This reverts commit ca94a5e114b87781fce754132b36f203bb6dbf92.

--- product/qcom-wifi.mk
+++ product/qcom-wifi.mk
@@ -2,7 +2,3 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
-
-# HIDL
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/hidl/manifest.xml:system/vendor/manifest.xml
