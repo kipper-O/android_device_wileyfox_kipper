f6147d1017f1ff05e1c148ddb8a684c0a5f56302
p-sdev <mahmod115.ma@gmail.com>
2017-12-29 11:10:29 +0300

    Add kernel flags

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,3 +1,6 @@
 # Kernel
+BOARD_DTBTOOL_ARGS := -2
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_CONFIG := lineageos_kipper_defconfig
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
